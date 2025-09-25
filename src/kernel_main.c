013b695d661e0a3171641fda1b9001cd9481c6ec
GrantSmith27 <gsmith12@luc.edu>
2025-09-24 22:27:59 -0500

    started

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,7 +1,14 @@
-
+#include "rprintf.h"
 #include <stdint.h>
 
-#define MULTIBOOT2_HEADER_MAGIC         0xe85250d6
+#define video_memory 0xB8000
+#define width 80
+#define height 25
+#define default_color 0x07
+
+static int row=0;
+static int column=0;
+
 
 const unsigned int multiboot_header[]  __attribute__((section(".multiboot"))) = {MULTIBOOT2_HEADER_MAGIC, 0, 16, -(16+MULTIBOOT2_HEADER_MAGIC), 0, 12};
 
@@ -11,15 +18,35 @@ uint8_t inb (uint16_t _port) {
     return rv;
 }
 
-void main() {
-    unsigned short *vram = (unsigned short*)0xb8000; // Base address of video mem
-    const unsigned char color = 7; // gray text on black background
-
-    while(1) {
-        uint8_t status = inb(0x64);
-
-        if(status & 1) {
-            uint8_t scancode = inb(0x60);
-        }
+void putc(int ch){
+   if (ch == '\n'){
+       row++;
+     } else {
+      int index = term_row * width + column;
+      video_memory[index] = (default_color <<8) | (unsigned char)ch;
+      column;
+      if(column >= width){
+        column=0;
+        row++;
+      }
     }
+
+
+   if (row >= height){
+
+  scroll();
+  row = height -1;
+
+     }
+}
+
+void scroll(){
+   for (int r =1; r < height, r++){
+     for (int c=0; c< width; c++){
+      video_memory[(r-1)*width + c] = video_memory[r*width + c];
+      }
+   }
+  for (int c=0; c < width; c++{
+    video_memory[(height-1)*width + c] = (default_color <<8 | ' ';
+  }
 }
